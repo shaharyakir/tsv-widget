aa09fefd3d9be9abd0afc0033c8a3606191b5123
Shahar Yakir <shahar.yakir@gmail.com>
2022-10-11 17:36:21 +0300

    highlightjs

--- src/lib/code-highlighter.ts
+++ src/lib/code-highlighter.ts
@@ -1,97 +1,41 @@
-import {loadWASM} from "onigasm"
-import {activateLanguage, addGrammar, addTheme, ITextmateThemePlus, linkInjections, setRoot} from "codemirror-textmate"
-import * as CodeMirror from "codemirror"
-// do not remove, this import makes sure the wasm file is included in the bundle
-import wasmBase64String from "./static/onigasm.wasm.js"
-import Utils from "./Utils.js"
+import hljs from "highlight.js";
+import hljsDefine from "highlightjs-func";
+import "highlight.js/styles/github.css";
 
+hljsDefine(hljs);
 /**
  *
  */
 export default class CodeHighlighter {
+  private editor: HTMLElement;
+  private readonly theme: string;
+  private tsvLanguageIndex: number = 0;
 
-    private editor: CodeMirror.EditorFromTextArea
-    private readonly theme: string
-    private tsvLanguageIndex: number = 0
+  /**
+   *
+   * @param theme
+   */
+  constructor(theme: string) {
+    this.theme = theme;
+  }
 
-    /**
-     *
-     * @param theme
-     */
-    constructor(theme: string) {
+  /**
+   *
+   * @param shadowRoot
+   * @param element
+   */
+  async init(shadowRoot: ShadowRoot, element: HTMLTextAreaElement) {
+    // Attach the created element to the shadow DOM
+    this.editor = element;
+  }
 
-        this.theme = theme
-
-    }
-
-    /**
-     *
-     * @param shadowRoot
-     * @param element
-     */
-    async init(shadowRoot: ShadowRoot, element: HTMLTextAreaElement) {
-
-        setRoot(shadowRoot)
-
-        const scopeName = 'source.func'
-        const language = 'func'
-
-        this.tsvLanguageIndex++
-
-        await loadWASM(
-            Utils.base64ToArrayBuffer(wasmBase64String)
-            // webpack has been configured to resolve `.wasm` files to actual 'paths" as opposed to using the built-in wasm-loader
-            // oniguruma is a low-level library and stock wasm-loader isn't equipped with advanced low-level API's to interact with libonig
-        )
-
-        await addGrammar(scopeName, () => import('./tm/grammar/func.tmLanguage.json') as any)
-
-        await activateLanguage(
-            scopeName,
-            language,
-            'now'
-        )
-
-        this.editor = CodeMirror.fromTextArea(element, {
-            lineNumbers: true,
-            // If you know in advance a language is going to be set on CodeMirror editor and it isn't preloaded by setting the third argument
-            // to `activateLanguage` to 'now', the contents of the editor would start of and remain as unhighlighted text, until loading is complete
-            mode: language
-        })
-
-        // Using Textmate theme in CodeMirror
-        let cmTheme = await import(
-            this.theme === 'dark' ? './tm/themes/dark.tmTheme.json' : './tm/themes/light.tmTheme.json'
-            )
-
-        const themeX: ITextmateThemePlus = {
-            ...cmTheme,
-            gutterSettings: cmTheme.gutterSettings
-        }
-
-        addTheme(themeX)
-
-        this.editor.setOption('theme', themeX.name)
-
-        const affectedLanguages = await linkInjections(scopeName, [scopeName])
-
-        console.log(affectedLanguages)
-        // You must re-trigger tokenization to apply the update above (if applicable)
-        const activeMode = this.editor.getOption('mode')
-        if (affectedLanguages.indexOf(activeMode.toString()) > -1) {
-            // Resetting cm's mode re-triggers tokenization of entire document
-            this.editor.setOption('mode', activeMode)
-        }
-    }
-
-    /**
-     *
-     * @param value
-     */
-    async setCode(value) {
-
-        return this.editor.setValue(value)
-
-    }
-
+  /**
+   *
+   * @param value
+   */
+  async setCode(value) {
+    this.editor.innerHTML = value;
+    hljs.highlightElement(this.editor);
+    hljs.highlightAll();
+  }
 }
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,126 +1,114 @@
-import CodeHighlighter from "./code-highlighter"
-import * as SVG from "./static/svg.json"
+import CodeHighlighter from "./code-highlighter";
+import * as SVG from "./static/svg.json";
 
-const style = require("./style.css")
+const style = require("./style.css");
 
 /**
  *
  */
 export default class TSVWidgetElement extends HTMLElement {
+  private ch: CodeHighlighter;
+  private sources: any = {};
+  private selectedFile: string = null;
+  private ipfsProvider: string;
+  private verifiedContractUrl: string;
+  private verifiedContract: any;
+  private theme: string;
+  private layout: string;
 
-    private ch: CodeHighlighter
-    private sources: any = {}
-    private selectedFile: string = null
-    private ipfsProvider: string
-    private verifiedContractUrl: string
-    private verifiedContract: any;
-    private theme: string;
-    private layout: string;
+  /**
+   *
+   */
+  constructor() {
+    super();
 
-    /**
-     *
-     */
-    constructor() {
+    this.attachShadow({ mode: "open" });
+  }
 
-        super()
+  /**
+   * Runs each time the element is appended to or moved in the DOM
+   */
+  async connectedCallback() {
+    console.log("connected!", this);
 
-        this.attachShadow({mode: 'open'})
+    this.ipfsProvider = this.getAttribute("ipfs-provider");
+    this.verifiedContractUrl = this.getAttribute("verified-contract-url");
+    this.theme = this.getAttribute("theme");
+    this.layout = this.getAttribute("layout");
 
-    }
+    this.ch = new CodeHighlighter(this.theme);
 
-    /**
-     * Runs each time the element is appended to or moved in the DOM
-     */
-    async connectedCallback() {
+    await this.buildContainer();
 
-        console.log('connected!', this);
+    await this.fetchSources();
 
-        this.ipfsProvider = this.getAttribute('ipfs-provider')
-        this.verifiedContractUrl = this.getAttribute('verified-contract-url')
-        this.theme = this.getAttribute('theme')
-        this.layout = this.getAttribute('layout')
+    await this.buildTabs();
 
-        this.ch = new CodeHighlighter(
-            this.theme
-        )
+    this.bindTabChangeEvents();
 
-        await this.buildContainer()
+    this.selectFile(this.selectedFile);
+  }
 
-        await this.fetchSources()
+  bindTabChangeEvents() {
+    const tabButtonsElements = this.shadowRoot.querySelectorAll(
+      "div.nav-tabs div.tab-button "
+    );
 
-        await this.buildTabs()
-
-        this.bindTabChangeEvents()
-
-        this.selectFile(
-            this.selectedFile
-        )
-
-    }
-
-    bindTabChangeEvents() {
-
-        const tabButtonsElements = this.shadowRoot.querySelectorAll("div.nav-tabs div.tab-button ");
-
-        for (const tabButtonsElement of tabButtonsElements) {
-
-            tabButtonsElement.addEventListener("click", () => {
-
-                for (const _tabButtonsElement of tabButtonsElements) {
-
-                    if (_tabButtonsElement !== tabButtonsElement) {
-                        _tabButtonsElement.classList.remove('active')
-                    }
-
-                }
-
-                tabButtonsElement.classList.add('active')
-
-                this.selectFile(tabButtonsElement.getAttribute('file-name'))
-            })
-
+    for (const tabButtonsElement of tabButtonsElements) {
+      tabButtonsElement.addEventListener("click", () => {
+        for (const _tabButtonsElement of tabButtonsElements) {
+          if (_tabButtonsElement !== tabButtonsElement) {
+            _tabButtonsElement.classList.remove("active");
+          }
         }
 
+        tabButtonsElement.classList.add("active");
+
+        this.selectFile(tabButtonsElement.getAttribute("file-name"));
+      });
     }
+  }
 
-    /**
-     * fetch sources from the verified contract json url
-     */
-    async fetchSources() {
+  /**
+   * fetch sources from the verified contract json url
+   */
+  async fetchSources() {
+    let url =
+      this.ipfsProvider +
+      "/ipfs/" +
+      this.verifiedContractUrl.replace("ipfs://", "");
 
-        let url = this.ipfsProvider + '/ipfs/' + this.verifiedContractUrl.replace('ipfs://', '')
+    this.verifiedContract = await (await fetch(url)).json();
 
-        this.verifiedContract = await (await fetch(url)).json()
+    await Promise.all(
+      this.verifiedContract.sources.map(async (source) => {
+        url = this.ipfsProvider + "/ipfs/" + source.url.replace("ipfs://", "");
 
-        await Promise.all(this.verifiedContract.sources.map(async source => {
+        const resp = await fetch(url);
 
-            url = this.ipfsProvider + '/ipfs/' + source.url.replace('ipfs://', '')
+        this.sources[source.fileName] = await resp.text();
+      })
+    );
 
-            const resp = await fetch(url)
+    this.loadingCurtain.classList.remove("visible");
+  }
 
-            this.sources[source.fileName] = await resp.text()
+  /**
+   * builds the external container element and appends it to the shadow root
+   */
+  async buildContainer() {
+    const dark = require("highlight.js/styles/atom-one-dark.css").toString();
+    const light = require("highlight.js/styles/atom-one-light.css").toString();
 
-        }));
+    const template = document.createElement("template");
 
-        this.loadingCurtain.classList.remove('visible')
-
-    }
-
-
-    /**
-     * builds the external container element and appends it to the shadow root
-     */
-    async buildContainer() {
-
-        const codeMirrorCss = (require("codemirror/lib/codemirror.css")).toString()
-
-        const template = document.createElement('template')
-
-        template.innerHTML = `      
-                <style>${codeMirrorCss}</style>
+    template.innerHTML = `      
+                <style>${this.theme === "dark" ? dark : light}</style>
                 <style>${style}</style>     
                 
-                <div part="container-tabs" class="container-tabs ${this.layout === 'vertical' ? 'vertical' : ''} ${this.theme === 'dark' ? 'dark' : ''}">
+                <div part="container-tabs" class="container-tabs ${
+                  this.layout === "vertical" ? "vertical" : ""
+                } ${this.theme === "dark" ? "dark" : ""}">
                     
                     <div id="loading-curtain" class="loading-curtain visible">
                     ${SVG.loadingAnimation}
@@ -129,98 +117,84 @@ export default class TSVWidgetElement extends HTMLElement {
                     <div part="tabs-buttons" id="tabs-buttons" class="nav nav-tabs">                       
                     </div>
                     <div part="tab-content" class="tab-content">
-                        <textarea class="cm-host-textarea" id="cm-host"></textarea>
+                        <pre><code class="language-func" id="code"></code></pre>
                     </div>
                 </div>           
-                `
+                `;
 
-        this.shadowRoot.appendChild(template.content.cloneNode(true))
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
 
-    }
+  /**
+   * builds the html tabs
+   */
+  async buildTabs() {
+    let tabsButtons = "";
 
-    /**
-     * builds the html tabs
-     */
-    async buildTabs() {
+    let index = 0;
 
-        let tabsButtons = ''
+    for (const sourceMetadata of this.verifiedContract.sources) {
+      const fileName = sourceMetadata.fileName;
 
-        let index = 0
-
-        for (const sourceMetadata of this.verifiedContract.sources) {
-
-            const fileName = sourceMetadata.fileName
-
-            tabsButtons += `<div part="tab-button" class="tab-button ${index === 0 ? 'active' : ''}" file-name="${fileName}">
+      tabsButtons += `<div part="tab-button" class="tab-button ${
+        index === 0 ? "active" : ""
+      }" file-name="${fileName}">
                                      ${fileName}
-                                    </div>`
+                                    </div>`;
 
-            if (index === 0) {
-                this.selectedFile = fileName
-            }
+      if (index === 0) {
+        this.selectedFile = fileName;
+      }
 
-            index++
-
-        }
-
-        this.tabsButtons.innerHTML = tabsButtons
-
-        await this.ch.init(
-            this.shadowRoot,
-            this.cmHost
-        )
-
+      index++;
     }
 
-    /**
-     * select a specific file and corresponding tab
-     *
-     * @param fileName
-     */
-    selectFile(fileName) {
+    this.tabsButtons.innerHTML = tabsButtons;
 
-        this.selectedFile = this.sources[fileName]
+    await this.ch.init(this.shadowRoot, this.cmHost);
+  }
 
-        this.ch.setCode(
-            this.selectedFile
-        )
+  /**
+   * select a specific file and corresponding tab
+   *
+   * @param fileName
+   */
+  selectFile(fileName) {
+    this.selectedFile = this.sources[fileName];
 
-    }
+    this.ch.setCode(this.selectedFile);
+  }
 
-    /**
-     * Runs when the element is removed from the DOM
-     */
-    disconnectedCallback() {
+  /**
+   * Runs when the element is removed from the DOM
+   */
+  disconnectedCallback() {
+    console.log("disconnected", this);
+  }
 
-        console.log('disconnected', this)
+  /**
+   *
+   */
+  get tabsButtons(): HTMLDivElement {
+    return this.shadowRoot.getElementById("tabs-buttons") as HTMLDivElement;
+  }
 
-    }
+  /**
+   *
+   */
+  get loadingCurtain(): HTMLDivElement {
+    return this.shadowRoot.getElementById("loading-curtain") as HTMLDivElement;
+  }
 
-    /**
-     *
-     */
-    get tabsButtons(): HTMLDivElement {
-        return this.shadowRoot.getElementById('tabs-buttons') as HTMLDivElement;
-    }
-
-    /**
-     *
-     */
-    get loadingCurtain(): HTMLDivElement {
-        return this.shadowRoot.getElementById('loading-curtain') as HTMLDivElement;
-    }
-
-    /**
-     *
-     */
-    get cmHost(): HTMLTextAreaElement {
-        return this.shadowRoot.getElementById('cm-host') as HTMLTextAreaElement;
-    }
-
+  /**
+   *
+   */
+  get cmHost(): HTMLTextAreaElement {
+    return this.shadowRoot.getElementById("code") as HTMLTextAreaElement;
+  }
 }
 
 // Define the new web component
-if ('customElements' in window) {
-    window.customElements.define('tsv-widget', TSVWidgetElement)
+if ("customElements" in window) {
+  window.customElements.define("tsv-widget", TSVWidgetElement);
 }
-
